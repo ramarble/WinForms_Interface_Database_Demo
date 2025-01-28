9d681d2fe0a597857838c9a91c1ecafd6331e6fd
raul@vagrantVM <raul.marrupe1@educa.madrid.org>
2025-01-28 00:55:21 +0000

    reportforms suck

--- DatabaseInterface/View/ReportForm.Designer.cs
+++ DatabaseInterface/View/ReportForm.Designer.cs
@@ -29,9 +29,14 @@
         private void InitializeComponent()
         {
             this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
-            this.button1 = new System.Windows.Forms.Button();
-            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.buttonUpdateFilters = new System.Windows.Forms.Button();
+            this.CategoryFilterCheckedBoxes = new System.Windows.Forms.CheckedListBox();
+            this.nudStock = new System.Windows.Forms.NumericUpDown();
+            this.labelStock = new System.Windows.Forms.Label();
+            this.labelPrice = new System.Windows.Forms.Label();
+            this.nudPrice = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.nudStock)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPrice)).BeginInit();
             this.SuspendLayout();
             // 
             // reportViewer1
@@ -40,48 +45,81 @@
             this.reportViewer1.Location = new System.Drawing.Point(12, 12);
             this.reportViewer1.Name = "reportViewer1";
             this.reportViewer1.ServerReport.BearerToken = null;
-            this.reportViewer1.Size = new System.Drawing.Size(518, 322);
+            this.reportViewer1.Size = new System.Drawing.Size(600, 379);
             this.reportViewer1.TabIndex = 0;
             // 
-            // button1
+            // buttonUpdateFilters
             // 
-            this.button1.Location = new System.Drawing.Point(13, 341);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(114, 44);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Buscar por DNI";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.buttonUpdateFilters.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonUpdateFilters.Location = new System.Drawing.Point(517, 500);
+            this.buttonUpdateFilters.Name = "buttonUpdateFilters";
+            this.buttonUpdateFilters.Size = new System.Drawing.Size(192, 44);
+            this.buttonUpdateFilters.TabIndex = 1;
+            this.buttonUpdateFilters.Text = "Filtrar por categoría";
+            this.buttonUpdateFilters.UseVisualStyleBackColor = true;
+            this.buttonUpdateFilters.Click += new System.EventHandler(this.button1_Click);
             // 
-            // maskedTextBox1
+            // CategoryFilterCheckedBoxes
             // 
-            this.maskedTextBox1.Location = new System.Drawing.Point(134, 353);
-            this.maskedTextBox1.Mask = "00000000";
-            this.maskedTextBox1.Name = "maskedTextBox1";
-            this.maskedTextBox1.Size = new System.Drawing.Size(100, 20);
-            this.maskedTextBox1.TabIndex = 2;
+            this.CategoryFilterCheckedBoxes.FormattingEnabled = true;
+            this.CategoryFilterCheckedBoxes.Location = new System.Drawing.Point(12, 500);
+            this.CategoryFilterCheckedBoxes.Name = "CategoryFilterCheckedBoxes";
+            this.CategoryFilterCheckedBoxes.Size = new System.Drawing.Size(120, 49);
+            this.CategoryFilterCheckedBoxes.TabIndex = 3;
             // 
-            // label1
+            // nudStock
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(267, 356);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(194, 13);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Componente extra no parte del ejercicio";
+            this.nudStock.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nudStock.Location = new System.Drawing.Point(138, 524);
+            this.nudStock.Name = "nudStock";
+            this.nudStock.Size = new System.Drawing.Size(124, 26);
+            this.nudStock.TabIndex = 4;
+            // 
+            // labelStock
+            // 
+            this.labelStock.AutoSize = true;
+            this.labelStock.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelStock.Location = new System.Drawing.Point(138, 500);
+            this.labelStock.Name = "labelStock";
+            this.labelStock.Size = new System.Drawing.Size(124, 20);
+            this.labelStock.TabIndex = 6;
+            this.labelStock.Text = "Stock superior a";
+            // 
+            // labelPrice
+            // 
+            this.labelPrice.AutoSize = true;
+            this.labelPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPrice.Location = new System.Drawing.Point(287, 500);
+            this.labelPrice.Name = "labelPrice";
+            this.labelPrice.Size = new System.Drawing.Size(127, 20);
+            this.labelPrice.TabIndex = 7;
+            this.labelPrice.Text = "Precio superior a";
+            // 
+            // nudPrice
+            // 
+            this.nudPrice.DecimalPlaces = 2;
+            this.nudPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nudPrice.Location = new System.Drawing.Point(290, 524);
+            this.nudPrice.Name = "nudPrice";
+            this.nudPrice.Size = new System.Drawing.Size(124, 26);
+            this.nudPrice.TabIndex = 8;
             // 
             // ReportForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(542, 397);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.maskedTextBox1);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(1584, 861);
+            this.Controls.Add(this.nudPrice);
+            this.Controls.Add(this.labelPrice);
+            this.Controls.Add(this.labelStock);
+            this.Controls.Add(this.nudStock);
+            this.Controls.Add(this.CategoryFilterCheckedBoxes);
+            this.Controls.Add(this.buttonUpdateFilters);
             this.Controls.Add(this.reportViewer1);
             this.Name = "ReportForm";
             this.Text = "ReportForm";
-            this.Load += new System.EventHandler(this.ReportForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudStock)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPrice)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -90,8 +128,11 @@
         #endregion
 
         private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonUpdateFilters;
+        private System.Windows.Forms.CheckedListBox CategoryFilterCheckedBoxes;
+        private System.Windows.Forms.NumericUpDown nudStock;
+        private System.Windows.Forms.Label labelStock;
+        private System.Windows.Forms.Label labelPrice;
+        private System.Windows.Forms.NumericUpDown nudPrice;
     }
 }
--- DatabaseInterface/View/ReportForm.cs
+++ DatabaseInterface/View/ReportForm.cs
@@ -6,42 +6,139 @@ using System.Windows.Forms;
 using Microsoft.Reporting.WinForms;
 using DatabaseInterfaceDemo.Model;
 using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.CodeDom;
 
 namespace DatabaseInterfaceDemo.View
 {
     public partial class ReportForm : Form
     {
+
+        static ReportDataSource r;
+        static ReportDataSource r2;
+        static BindingList<object> ListCurrentlyInUse;
+        static BindingList<object> InitialList;
+        static int PADDING_LEFT = 12;
+        static int PADDING_RIGHT = 42;
+        static int PADDING_BOTTOM = 42;
+        static int PADDING_TOP = 12;
+        static int PADDING_BUTTONS = 112;
         public ReportForm(BindingList<object> list)
         {
             InitializeComponent();
-            objectList = list;
+
+            InitialList = list;
+            ListCurrentlyInUse = list;
+            CategoryFilterCheckedBoxes.Items.AddRange(Enum.GetNames(typeof(Category)));
+
+            nudStock.Controls.RemoveAt(0);
+            nudPrice.Controls.RemoveAt(0);
+
+            ProgramaticallyPlaceObjects(null, null);
+            InitializeReportViewer();
+            this.ResizeEnd += ProgramaticallyPlaceObjects;
         }
 
-        BindingList<object> objectList;
-
-        private void ReportForm_Load(object sender, EventArgs e)
+        private void InitializeReportViewer()
         {
             this.reportViewer1.LocalReport.ReportPath = "../../View/Report1.rdlc";
-            ReportDataSource r = new ReportDataSource("DataSet1", objectList);
+
+
+            r = new ReportDataSource("Producto_DataSet", ListCurrentlyInUse);
             this.reportViewer1.LocalReport.DataSources.Add(r);
             this.reportViewer1.RefreshReport();
+        }
+
+        private void ProgramaticallyPlaceObjects(object sender, EventArgs e)
+        {
+            this.StartPosition = FormStartPosition.Manual;
+            reportViewer1.Location = new Point(PADDING_LEFT, PADDING_TOP);
+            reportViewer1.Size = new Size(this.Size.Width - PADDING_RIGHT, this.Size.Height - PADDING_BUTTONS);
+
+            CategoryFilterCheckedBoxes.Location = new Point(
+                PADDING_LEFT, 
+                Size.Height - PADDING_BOTTOM - CategoryFilterCheckedBoxes.Size.Height);
+
+            nudStock.Location = new Point(
+                PADDING_LEFT + CategoryFilterCheckedBoxes.Location.X + CategoryFilterCheckedBoxes.Width,
+                Size.Height - PADDING_BOTTOM - nudStock.Size.Height);
+
+            labelStock.Location = new Point(
+                nudStock.Location.X,
+                Size.Height - PADDING_BOTTOM - labelStock.Size.Height - nudStock.Size.Height);
+            
+            
+            nudPrice.Location = new Point(
+                PADDING_LEFT + nudStock.Location.X + nudStock.Width, 
+                Size.Height - PADDING_BOTTOM - nudPrice.Size.Height);
+
+            labelPrice.Location = new Point(
+                nudPrice.Location.X,
+                Size.Height - PADDING_BOTTOM - labelPrice.Size.Height - nudPrice.Size.Height);
+
+            buttonUpdateFilters.Location = new Point(
+                Size.Width - buttonUpdateFilters.Width - PADDING_RIGHT,
+                Size.Height - PADDING_BOTTOM - buttonUpdateFilters.Height);
 
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (maskedTextBox1.Text.ToString() == "")
-            {
-                ReportForm_Load(null, null);
-            }
-            else
-            {
-                //THIS WON'T WORK FOR NOW
-                this.reportViewer1.LocalReport.DataSources.Clear();
-                //var newUserList = objectList.Where(it => it.nif.ToString().Contains(maskedTextBox1.Text.ToString()));
-                //this.reportViewer1.LocalReport.DataSources.Add(new ReportDataSource("DataSet1", newUserList));
-                //this.reportViewer1.RefreshReport();
-            }
+            this.reportViewer1.LocalReport.DataSources.Remove(r);
+            ListCurrentlyInUse = UpdateListBasedOnCategoryFilter(InitialList);
+            ListCurrentlyInUse = UpdateListBasedOnPriceFilter(ListCurrentlyInUse);
+            ListCurrentlyInUse = UpdateListBasedOnStockFilter(ListCurrentlyInUse);
+
+
+            r = new ReportDataSource("Producto_DataSet", ListCurrentlyInUse);
+
+
+            this.reportViewer1.LocalReport.DataSources.Add(r);
+            this.reportViewer1.RefreshReport();
         }
+
+
+        private BindingList<object> UpdateListBasedOnCategoryFilter(BindingList<object> listWorkedOn)
+        {
+            if (CategoryFilterCheckedBoxes.CheckedItems.Count > 0)
+            {
+                List<object> slice = new List<object>();
+                foreach (var c in CategoryFilterCheckedBoxes.CheckedItems)
+                {
+                    slice.AddRange(listWorkedOn.Where(it => (it as Producto).Category == (Category)Enum.Parse(typeof(Category), c.ToString())));
+                }
+                return new BindingList<object>(slice);
+            }
+            else return listWorkedOn;
+        }
+
+        private BindingList<object> UpdateListBasedOnStockFilter(BindingList<object> listWorkedOn)
+        {
+
+            if (nudStock.Value > 0)
+            {
+                List<object> slice = new List<object>();
+                slice.AddRange(listWorkedOn.Where(it => (it as Producto).Stock > nudStock.Value));
+
+                return new BindingList<object>(slice);
+            }
+            else return listWorkedOn;
+
+        }
+
+        private BindingList<object> UpdateListBasedOnPriceFilter(BindingList<object> listWorkedOn)
+        {
+            if (nudPrice.Value > 0)
+            {
+                List<object> slice = new List<object>();
+                slice.AddRange(listWorkedOn.Where(it => nudPrice.Value < (it as Producto).PricePerUnit));
+
+
+                return new BindingList<object>(slice);
+            }
+            else return listWorkedOn;
+        }
+
     }
 }
